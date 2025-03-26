a7c6de83eef04e96ac46b95efcf1b8ac92ff9a95
ghousepasha67 <144920374+ghousepasha67@users.noreply.github.com>
2025-03-26 18:27:33 +0530

    Add files via upload

---
+++ src/Pages/ImagePath.jsx
@@ -0,0 +1,24 @@
+import cola from '../assets//img/coco-cola.png';
+import carrerBanner from '../assets/img/career-banner.png';
+import case1 from '../assets/img/case-1.png';
+import cube from '../assets/img/cube.png';
+import FooterLogo from '../assets/img/footer-logo.svg';
+import Ibm from '../assets/img/IBM.png';
+import key1 from '../assets/img/Key1.svg';
+import key2 from '../assets/img/Key2.svg';
+import key3 from '../assets/img/Key3.svg';
+import key4 from '../assets/img/Key4.svg';
+import logoBlue from '../assets/img/logo-blue.svg';
+import image from '../assets/img/logo.jpg';
+import logo from '../assets/img/Logo2.svg';
+import testimonial from '../assets/img/testimonial.png';
+
+
+
+export {
+    carrerBanner, case1, cola, cube, FooterLogo, Ibm, image, key1,
+    key2,
+    key3,
+    key4, logo, logoBlue, testimonial
+};
+
